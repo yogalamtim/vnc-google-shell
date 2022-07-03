13e0c65353dfd03ab75e575a355384855f313b7e
yogalamtim <33374451+yogalamtim@users.noreply.github.com>
2022-07-03 19:19:21 +0700

    Update vnc2.py

--- vnc2.py
+++ vnc2.py
@@ -4,7 +4,7 @@ cmd = 'sudo apt update'
 os.system(cmd)
 cmd = 'sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb'
 os.system(cmd)
-cmd = 'sudo apt install --assume-yes xfce4 desktop-base'
+cmd = 'sudo apt install --assume-yes xfce4 desktop-base dbus-x11'
 os.system(cmd)
 cmd = 'sudo apt install xfce4-terminal -y'
 os.system(cmd)
