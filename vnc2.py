b4a4078aab91829f5b74beea3d79d2f484b1f0d6
yogalamtim <33374451+yogalamtim@users.noreply.github.com>
2022-07-03 19:03:17 +0700

    Create vnc2.py

---
+++ vnc2.py
@@ -0,0 +1,27 @@
+import os
+
+cmd = 'sudo apt update'
+os.system(cmd)
+cmd = 'sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb'
+os.system(cmd)
+cmd = 'sudo apt install --assume-yes xfce4 desktop-base'
+os.system(cmd)
+cmd = 'sudo apt install xfce4-terminal -y'
+os.system(cmd)
+cmd = 'sudo apt install firefox-esr -y'
+os.system(cmd)
+cmd = 'sudo apt install actionaz -y'
+os.system(cmd)
+cmd = 'wget https://github.com/tettapterb/tettapterb/releases/download/berjas/slimjet_amd64.deb'
+os.system(cmd)
+cmd = 'sudo apt-get install --assume-yes ./slimjet_amd64.deb'
+os.system(cmd)
+cmd = 'sudo apt-get -f install -y'
+os.system(cmd)
+cmd = 'sudo apt-get install geany -y'
+os.system(cmd)
+cmd = 'sudo apt-get install vim-gtk3 -y'
+os.system(cmd)
+cmd = 'sudo apt install iputils-ping -y'
+os.sytem(cmd)
+print("vnc up")
