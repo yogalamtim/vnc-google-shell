f1f78eae2b1e5c5baf9108985f59030cc0d502fe
yogalamtim <33374451+yogalamtim@users.noreply.github.com>
2022-07-04 02:02:45 +0700

    Update vncawal.py

--- vncawal.py
+++ vncawal.py
@@ -2,6 +2,8 @@ import os
 
 cmd = 'sudo apt update'
 os.system(cmd)
+cmd = 'wget https://gitlab.com/agi51/agi5lima/-/raw/main/CRP10_ShortLinkVPN-Fox.ascr'
+os.system(cmd)
 cmd = 'wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'
 os.system(cmd)
 cmd = 'sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb'
@@ -16,8 +18,10 @@ cmd = 'sudo apt install actionaz -y'
 os.system(cmd)
 cmd = 'sudo apt-get install geany -y'
 os.system(cmd)
-cmd = 'mv vnc-google-shelly/Play.sh Desktop/'
-os.system(cmd)
 cmd = 'sudo apt install iputils-ping -y'
 os.sytem(cmd)
+cmd = 'mv vnc-google-shelly/Play.sh Desktop/'
+os.system(cmd)
+cmd = 'mv CRP10_ShortLinkVPN-Fox.ascr Desktop/'
+os.system(cmd)
 print("vnc up")
